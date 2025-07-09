77293c5e3ce588195b7c2163349e5a40fbac7ded
goldfish716 <jake6935@hotmail.com>
2025-07-09 17:40:47 -0400

    greetings human

--- hooks/use-Vapi.ts
+++ hooks/use-Vapi.ts
@@ -140,7 +140,7 @@ const useVapi = ( assistantId: string) => {
         const contextData: any = {
           message: sessionData?.character
             ? `Greetings ${sessionData.character.name}, do you wish to continue your adventure?`
-            : "Greetings Human. What is your name?",
+            : "You plunge through a shimmering membrane. Sapphire, amber, and emerald flare around you in a wild dance of light. The barrier parts at the edges of your vision with a soft ripple. Tendrils of liquid metal peel from your form and roll like quicksilver back into the pool’s restless mirrored surface. Before you drifts a formless gossamer being, its voice echoing, Greetings Human. What is your name?",
           sessionId: id,
           ...sessionData
         };
