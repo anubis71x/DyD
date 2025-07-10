859e303cda07478302c375f9b7c85c191de3e9db
goldfish716 <jake6935@hotmail.com>
2025-07-09 22:15:22 -0400

    refining first message text

--- hooks/use-Vapi.ts
+++ hooks/use-Vapi.ts
@@ -132,7 +132,7 @@ const useVapi = (assistantId: string) => {
         }
 
         const firstMessage = sessionData?.character
-          ? `Greetings ${sessionData.character.name}, do you wish to continue your adventure?`
+          ? `You pass through the shimmering dreampool with a soft ripple. A gossamer being floats before you. Greetings ${sessionData.character.name}, do you wish to continue your adventure?`
           : "You plunge through a shimmering membrane. Sapphire, amber, and emerald flare around you in a wild dance of light. The barrier parts at the edges of your vision with a soft ripple. Tendrils of liquid metal peel from your form and roll like quicksilver back into the pool’s restless mirrored surface. Before you drifts a formless gossamer being, its voice echoing, Greetings traveler. This is a world between worlds, an existence between existences. What is your name? And what existence do you seek to embody?";
 
         const contextData: any = {
